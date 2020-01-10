0bab5768e85686f37b8d570361b2c3461de99584
Andrew Jacob <amj1108@icloud.com>
2020-01-09 17:11:35 -0700

    make app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/home'
+import Shop from './pages/shop'
+import Cart from './pages/cart'
+import Product from './pages/product'
+import NotFound from './pages/404'
+
+import Header from './components/header'
+import Footer from './components/footer'
+
+import reducers from './store/reducers'
+const store = createStore(reducers)
+
+class App extends React.Component {
+	render() {
+		return(<Provider store={store}>
+			<Router>
+				<Header/>
+				<Switch>
+					<Route exact path="/"><Home/></Route>
+					<Route exact path="/shop"><Shop/></Route>
+					<Route path="/cart"><Cart/></Route>
+					<Route path="/shop/:id" component={Product}/>
+					<Route><NotFound/></Route>
+				</Switch>
+				<Footer/>
+			</Router>
+		</Provider>)
+	}
 }
 
-export default App;
+export default App
